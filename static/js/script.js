468b9b64462e62fb766d8c7cddf3bb917f6c0cd8
kunle-lawal <olawal196@gmail.com>
2020-02-06 22:23:29 -0500

    Added social icon animation.

--- static/js/script.js
+++ static/js/script.js
@@ -47,8 +47,11 @@ $(".page").scroll(function () {
 
 window.setInterval(function() {
     let pageHeight = $('.page').height();
+    let footerHeight = $('footer').height();
     let pageScrollHeight = $('.page')[0].scrollHeight;
     let scrollPosition = $('.page').scrollTop();
+    footerHeight = ((footerHeight + 40) / $(window).height()) * 100;
+    console.log(footerHeight);
     // console.log(`${Math.round(pageScrollHeight - pageHeight) - 10} <=  ${scrollPosition}`);
     scrolling = false;
     if (Math.round(pageScrollHeight - pageHeight) - 10 <= scrollPosition && !hitBottom) {
@@ -57,17 +60,17 @@ window.setInterval(function() {
             targets: '.page',
             easing: 'easeInOutBack',
             duration: 450,
-            bottom: (size == 'xlarge' || size == 'large') ? ['5%', '20%'] : '15vh',
+            bottom: (size == 'xlarge' || size == 'large') ? ['5%', (footerHeight) + '%'] : (footerHeight) + '%',
             delay: 100,
             complete: function () {
                 $('footer').css('opacity', '1');
                 anime({
                     targets: '#fa',
-                    easing: 'easeInOutBack',
-                    // marginTop: ['0px', '20px'],
-                    duration: 550,
+                    easing: 'spring',
+                    marginTop: ['0px', '-20px', '10px', '0px'],
+                    duration: 1200,
                     // delay: 1000,
-                    delay: anime.stagger(200, { from: 'center' }),
+                    delay: anime.stagger(100, { from: 'center' }),
                 })
             }
         })
